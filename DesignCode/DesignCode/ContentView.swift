83815f81538af917aae13e6cb6e45301c3afd1d2
Vidit Shah <0210vidit@gmail.com>
2020-06-04 23:29:56 +0530

    designCode

---
+++ DesignCode/DesignCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by vidit shah on 04/06/20.
+//  Copyright © 2020 vidit shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+let color ="green";
+
