4a59be74369caf119ac004539b1fcc3dad5c1f4a
Vidit Shah <0210vidit@gmail.com>
2020-06-05 22:11:17 +0530

    dc-swift-2

--- DesignCode/DesignCode/ContentView.swift
+++ DesignCode/DesignCode/ContentView.swift
@@ -14,20 +14,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            BackCardView()
-             .offset(x: 0, y: -40)
-                .scaleEffect(0.9)
-                .rotationEffect(.degrees(10))
-            
-            BackCardView()
-                .offset(x: 0, y: -20)
-                .scaleEffect(0.95)
-                .rotationEffect(Angle(degrees:5))
-            
-
-
-            //Last card is in front
-            CardView()
+            VStack {
+                Spacer()
+            }.frame(width: 300, height: 220).background(Color.blue).cornerRadius(20).shadow(radius:20).offset(x: 0, y: -20)
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.title)
+                            .fontWeight(.semibold).foregroundColor(.white)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
+                    }.padding(20)
+                    Spacer()
+                    Image("Logo1")
+                }
+                Image("Card1").resizable().aspectRatio(contentMode: .fill).frame(width: 300, height: 110,alignment: .top)
+            }
+            .frame(width: 340.0, height: 220.0).background(Color.black).cornerRadius(20).shadow(radius: 20)
         }
     }
 }
@@ -38,35 +42,9 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
  
+ 
 
-struct CardView: View {
-    var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("UI Design")
-                        .font(.title)
-                        .fontWeight(.semibold).foregroundColor(.white)
-                    Text("Certificate")
-                        .foregroundColor(Color("accent"))
-                }.padding(20)
-                Spacer()
-                Image("Logo1")
-            }
-            Image("Card1").resizable().aspectRatio(contentMode: .fill).frame(width: 300, height: 110,alignment: .top)
-        }
-        .frame(width: 340.0, height: 220.0).background(Color.black).cornerRadius(20).shadow(radius: 20)
-    }
-}
 
-struct BackCardView: View {
-    var body: some View {
-        VStack {
-            Spacer()
-        }.frame(width: 340, height: 220)
-            .background(Color.blue)
-            .cornerRadius(20)
-            .shadow(radius:20)
-           
-    }
-}
+ 
+
+
--- Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Control + arrow bringing in the reference
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageViewOne.image=#imageLiteral(resourceName: "DiceSix") //image Literal
+        diceImageViewTwo.image=#imageLiteral(resourceName: "DiceTwo")
+        
     }
 
-
+    
 }
 
