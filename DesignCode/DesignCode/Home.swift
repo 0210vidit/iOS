be9d9d151ce184402b8e65ceea00dd80a92ec390
Vidit Shah <0210vidit@gmail.com>
2020-06-06 20:15:33 +0530

    HomeView

---
+++ DesignCode/DesignCode/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  DesignCode
+//
+//  Created by vidit shah on 06/06/20.
+//  Copyright © 2020 vidit shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Watching")
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
